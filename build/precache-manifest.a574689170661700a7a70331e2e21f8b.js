4a78b0a5c8951a16506f2d20556f974181080a1b
edward <zyclub@126.com>
2019-07-02 13:49:43 +0800

    build提交

---
+++ build/precache-manifest.a574689170661700a7a70331e2e21f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06ce44f3b3239cb8864f9ffa772ab509",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa956b7e98aaa9440873",
+    "url": "./static/css/main.f316d966.chunk.css"
+  },
+  {
+    "revision": "f5470452aec0bd61a447",
+    "url": "./static/js/2.4cbfd1a3.chunk.js"
+  },
+  {
+    "revision": "aa956b7e98aaa9440873",
+    "url": "./static/js/main.28f964a1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f1d4564f00bf998a440a92bbc3c97945",
+    "url": "./static/media/homeBG1.f1d4564f.jpeg"
+  }
+]);
